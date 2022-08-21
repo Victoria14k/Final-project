3c06dc9cf03c39e8c275dd5c6d2b021ec01350d4
Victoria14k <108625026+Victoria14k@users.noreply.github.com>
2022-08-21 22:48:50 +0300

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
 console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
@@ -6,13 +21,14 @@ let descriptionElement = document.querySelector("#description");
 let feelslikeElement = document.querySelector("#feels-like");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#windspeed");
+let dateElement = document.querySelector("#date");
 cityElement.innerHTML = response.data.name;
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 descriptionElement.innerHTML = response.data.weather[0].description;
 feelslikeElement.innerHTML = Math.round(response.data.main.feels_like);
-humidityElement = response.data.main.humidity;
-windElement = Math.round(response.data.wind.speed);
-
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "a2095828db09eb62d8b590f3de4c1377";
