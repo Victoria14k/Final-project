9e2cecaeb9785f5f2197e9028eae9887d1167035
Victoria14k <108625026+Victoria14k@users.noreply.github.com>
2022-08-24 23:15:00 +0300

    Injected HTML into JS to create forecast

--- src/app.js
+++ src/app.js
@@ -13,6 +13,40 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function(day){
+        forecastHTML =
+          forecastHTML +
+          `
+                <div class="col-2">
+                    <div class="weather-forecast-date">${day}</div>
+                    <img 
+                    src="http://openweathermap.org/img/wn/01d@2x.png" 
+                    alt=""
+                    width="42"
+                    />
+                    <div class="weather-forecast-temperature">
+                       <span class="weather-forecast-temperature-max">30º</span>
+                       <span class="weather-forecast-temperature-min">22º</span>
+                    </div>
+                </div>
+            
+            `;
+    }) 
+
+    
+
+    
+    forecastHTML = forecastHTML + `</div>`;       
+    forecastElement.innerHTML = forecastHTML;
+
+}
+
+
 function displayTemperature(response){
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
@@ -81,7 +115,7 @@ function displayCelsiusTemperature(event){
 let celsiusTemperature = null;
 
 
-
+displayForecast();
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
